4732112415de5637a30a6cdc8c5aa28d0111b6b3
limyunkai19 <yunkai96@hotmail.com>
2018-07-03 12:33:24 +0800

    Add static ip

--- README.md
+++ README.md
@@ -20,3 +20,6 @@ enable_ssh | enable/disable ssh on your Raspberry Pi | `true` (enable ssh) or `f
 locale     | set the locale of your Raspberry Pi | if you are not sure for this one, it is recommended to leave it as the default | `en_US.UTF-8`
 timezone   | set the timezone of your Raspberry Pi | find your timezone at [here](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) or use `pistrap/find_timezone.sh`. Some timezone example are `Asia/Kuala_Lumpur`, `America/New_York` | `Etc/UTC`
 install_zsh| install my favorite shell, _zsh_ with _oh-my-zsh_ and set _zsh_ as default shell | `true` (install _zsh_) or `false` don't install _zsh_. _zsh_ are great but installing it may take some time and internet connection. | `false`
+
+## To Do
+- [ ] Configure keyboard for GUI user
--- pistrap.sh
+++ pistrap.sh
@@ -24,7 +24,14 @@ echo -e "hostname:\t$hostname"
 echo -ne "enable ssh:\t" && ( [ "$enable_ssh" = true ] && echo "yes" || echo "no" )
 echo -e "locale:\t\t$locale"
 echo -e "timezone:\t$timezone"
-echo -ne "install zsh:\t" && ( [ "$enable_ssh" = true ] && echo "yes" || echo "no" )
+echo -ne "install zsh:\t" && ( [ "$install_zsh" = true ] && echo "yes" || echo "no" )
+echo -ne "static ip:\t" && ( [ "$static_ip" = true ] && echo "yes" || echo "no" )
+if [ "$static_ip" = true ]; then
+    echo "    interface $interface"
+    echo "    static ip_address=$static_ip_address"
+    echo "    static routers=$static_routers"
+    echo "    static domain_name_servers=$static_dns"
+fi
 echo
 read -p "Using the above setting, do you want to continue [Y/n]? " -r
 echo
@@ -38,7 +45,7 @@ echo "Configuring locale, timezone and ssh"
 raspi-config nonint do_change_locale $locale &>/dev/null
 raspi-config nonint do_change_timezone $timezone &>/dev/null
 
-## ssh
+# ssh
 if [ "$enable_ssh" = true ]; then
     raspi-config nonint do_ssh 0 &>/dev/null
 else
@@ -47,17 +54,37 @@ fi
 
 # install zsh
 if [ "$install_zsh" = true ]; then
-    echo "Installing zsh, this may take some time"
-    apt-get update &>/dev/null
-    apt-get install -y zsh git &>/dev/null
-    chsh -s $(which zsh) $SUDO_USER
+    if [ ! -d $(getent passwd $SUDO_USER | cut -d: -f6)/.oh-my-zsh ]; then
+        echo "Zsh installed, not installing"
+    else
+        echo "Installing zsh, this may take some time"
+        apt-get update &>/dev/null
+        apt-get install -y zsh git &>/dev/null
+        chsh -s $(which zsh) $SUDO_USER
 
-    echo "Installing oh-my-zsh"
-    su - $SUDO_USER -c '0>/dev/null sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"' &>/dev/null
+        echo "Installing oh-my-zsh"
+        su - $SUDO_USER -c '0>/dev/null sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"' &>/dev/null
+    fi
 fi
 
-
 ## need to reboot
+# static ip
+cp raspbian_conf/dhcpcd.conf /etc/dhcpcd.conf
+if [ "$static_ip" = true ]; then
+    echo "Configuring static ip address"
+    metric=100
+    for int in $interface; do
+        echo "interface $int" >> /etc/dhcpcd.conf
+        echo "metric $metric" >> /etc/dhcpcd.conf
+        echo "static ip_address=$static_ip_address" >> /etc/dhcpcd.conf
+        echo "static routers=$static_routers" >> /etc/dhcpcd.conf
+        echo "static domain_name_servers=$static_dns" >> /etc/dhcpcd.conf
+        echo "" >> /etc/dhcpcd.conf
+
+        metric=$((metric + 100))
+    done
+fi
+
 # configure host name
 echo "Configuring hostname"
 raspi-config nonint do_hostname $hostname
@@ -69,10 +96,3 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
     reboot
 fi
-
-
-###### to do ######
-# ip stuff
-
-# configure keyboard
-# for gui user, ssh can ignore
